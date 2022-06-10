c246667363e8bfa71a0ca5051d68060f5ce31e7e
Lex Krooswijk <lexkrooswijk@live.nl>
2022-06-10 12:46:24 +0200

    actions test

--- WorkoutService/Controllers/ExerciseController.cs
+++ WorkoutService/Controllers/ExerciseController.cs
@@ -15,7 +15,7 @@ namespace WorkoutService.Controllers
         {
             _exerciseService = exerciseService;
 
-            // Actions tries: 2
+            // Actions tries: 3
         }
 
         [HttpPost]
