903b8aa22f4b1ee8e72b53b321f82447d158f201
Lex Krooswijk <lexkrooswijk@live.nl>
2022-06-18 22:47:46 +0200

    github actions run

--- ApiGateway/Program.cs
+++ ApiGateway/Program.cs
@@ -1,7 +1,10 @@
 using System.IO;
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Tokens;
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
 
@@ -16,15 +19,31 @@ namespace ApiGateway
             .UseContentRoot(Directory.GetCurrentDirectory())
             .ConfigureAppConfiguration((hostingContext, config) =>
             {
+                var env = hostingContext.HostingEnvironment.EnvironmentName;
+
                 config
                     .SetBasePath(hostingContext.HostingEnvironment.ContentRootPath)
                     .AddJsonFile("appsettings.json", true, true)
-                    .AddJsonFile($"appsettings.{hostingContext.HostingEnvironment.EnvironmentName}.json", true, true)
-                    .AddJsonFile("ocelot.json")
+                    .AddJsonFile($"appsettings.{env}.json", true, true)
+                    .AddJsonFile($"ocelot.{env}.json")
                     .AddEnvironmentVariables();
             })
             .ConfigureServices(s => {
                 s.AddOcelot();
+                s.AddAuthentication(options => {
+                    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                }).AddJwtBearer(options => {
+                    options.RequireHttpsMetadata = false;
+                    options.SaveToken = true;
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("_G_:hE7Q(f5nH9~-m&pX$&CgJjfuqQ")),
+                        ValidateIssuerSigningKey = true,
+                        ValidateIssuer = false,
+                        ValidateAudience = false
+                    };
+                });
             })
             .ConfigureLogging((hostingContext, logging) =>
             {
@@ -34,6 +53,7 @@ namespace ApiGateway
             .Configure(app =>
             {
                 app.UseOcelot().Wait();
+                app.UseAuthentication();
             })
             .Build()
             .Run();
